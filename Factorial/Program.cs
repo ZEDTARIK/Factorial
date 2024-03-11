21c13fc21dfab5fb9dd436eeb6382dd725054a59
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-03-11 14:54:41 +0000

    Factorial en Csharp

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number(Factorial) : ");
+            Int32 nbFactorial = Int32.Parse(Console.ReadLine());
+            int result = 1;
+            for(int i=1; i<=nbFactorial; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("\nThe result of Factorial {0}! is : {1} ", nbFactorial, result);
+
+            Console.ReadKey();
+        }
+    }
+}
